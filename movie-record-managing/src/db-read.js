9993270441ea337dce18cb0995569e6a75d1ceda
JJJJEAR <110074209+JJJJEAR@users.noreply.github.com>
2023-10-23 02:42:52 +0700

    Update db-read.js

--- movie-record-managing/src/db-read.js
+++ movie-record-managing/src/db-read.js
@@ -1,59 +1,55 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import './table-style.css'
 
 export default function DBRead() {
     let [data, setData] = React.useState('')
 
-    React.useEffect(() => {	
-		fetch('/api/db/read')
-		.then(response => response.json())
-		.then(result => {       
-            if (result.length > 0) {
-                showData(result)
-            } else {
-                setData(<>ไม่มีรายการข้อมูล</>)
-            }   
-		})
-        .catch(err => alert(err))        
+    React.useEffect(() => {
+        fetch('/api/db/read')
+            .then(response => response.json())
+            .then(result => {
+                if (result.length > 0) {
+                    showData(result)
+                } else {
+                    setData(<>ไม่มีรายการข้อมูล</>)
+                }
+            })
+            .catch(err => alert(err))
     }, [])
 
     const showData = (result) => {
         let tb = (
-        <table>
-        <tr>
-            <th class="thLeft">ชื่อสินค้า</th><th>ราคา</th>
-            <th>วันที่เพิ่มสินค้า</th><th class="thLeft">รายละเอียด</th>
-        </tr>
-        {
-        result.map(doc => {
-            //จัดรูปแบบวันเดือนปี ที่สามารถเข้าใจได้
-            let dt = new Date(Date.parse(doc.date_added))
-            let df = (
-                <>{dt.getDate()}-{dt.getMonth()+1}-{dt.getFullYear()}</>
-            )
-            let p = new Intl.NumberFormat().format(doc.price)
-            
-            return (
-            <tr>
-                <td>{doc.name}</td>
-                <td class="tdCenter">{p}</td>
-                <td class="tdCenter">{df}</td>
-                <td>{doc.detail}</td>
-            </tr>
-            )
-        })
-        }
-        </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th className="thLeft">Movie Title</th>
+                        <th>Year Release</th>
+                        <th className="thLeft">Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                    result.map(doc => {
+                        return (
+                            <tr key={doc._id}>
+                                <td>{doc.movie_title}</td>
+                                <td className="tdCenter">{doc.YearRL}</td>
+                                <td>{doc.Rate}</td>
+                            </tr>
+                        )
+                    })
+                    }
+                </tbody>
+            </table>
         )
-    
+
         setData(tb)
     }
 
     return (
-        <div style={{margin:'20px'}}>
+        <div style={{ margin: '20px' }}>
             <div id="data">{data}</div>
-            <br/>
+            <br />
             <a href="/db">หน้าหลัก</a>
         </div>
     )
