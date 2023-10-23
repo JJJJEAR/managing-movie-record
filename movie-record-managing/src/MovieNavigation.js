e739dd66857335bb61ab83785baf7e86714e7ced
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-10-24 05:56:37 +0700

    Update MovieNavigation.js

--- movie-record-managing/src/MovieNavigation.js
+++ movie-record-managing/src/MovieNavigation.js
@@ -17,18 +17,6 @@ export default function MovieNavigation() {
                 <NavLink to="/" className={({ isActive }) => isActive ? "active_menu" : "menu" } >
                     Movie
                 </NavLink>&nbsp;
-                <NavLink to="/Create" className={({ isActive }) => isActive ? "active_menu" : "menu" } >
-                    Create
-                </NavLink>&nbsp;
-                <NavLink to="/Read" className={({ isActive }) =>  isActive ? "active_menu" : "menu" } >
-                    Read
-                </NavLink>&nbsp;
-                <NavLink to="/Update" className={({ isActive }) => isActive ? "active_menu" : "menu" } >
-                    Update
-                </NavLink>&nbsp;
-                <NavLink to="/Delete" className={({ isActive }) => isActive ? "active_menu" : "menu" } >
-                    Delete
-                </NavLink>
                 <NavLink to="/Register" className={({ isActive }) => isActive ? "active_menu" : "menu" } >
                     Register
                 </NavLink>
@@ -40,10 +28,6 @@ export default function MovieNavigation() {
             <Routes style={{ margin: "50px" }}>
                 <Route path="/" element={<MovieLists/>} />
                 <Route path="/Movie" element={<MovieLists />} />
-                <Route path="/Create" element={<DBCreate />} />
-                <Route path="/Read" element={<DBRead />} />
-                <Route path="/Update" element={<DBUpdate />} />
-                <Route path="/Delete" element={<DBDelete />} />
                 <Route path="*" element={<Navigate to="/" />} />
                 <Route path="/Register" element={<Register/>} />
                 <Route path="/Login" element={<Login/>} />
