88fb165dfdaf265f8e9e00be5bbebfc9784ce918
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-10-24 04:41:55 +0700

    Update Register.js

--- movie-record-managing/src/Register.js
+++ movie-record-managing/src/Register.js
@@ -3,48 +3,50 @@ import './style.css'
 
 export default function Register() {
     const form = React.useRef()
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [role, setRole] = React.useState(''); 
+    const [role, setRole] = React.useState('MANAGER');
 
-    const roles = ['MANAGER', 'TEAMLEADER', 'FLOORSTAFF']; 
-    const handleRegister = async (event) => {
+    const roles = ['MANAGER', 'TEAMLEADER', 'FLOORSTAFF'];
+    const onSubmitForm = async (event) => {
         event.preventDefault();
-      
-        const fd = new FormData(form.current);
-        console.log(fd);
-      
-        fetch('/api/db/register', {
-          method: 'POST',
-          body: fd,
-        })
-          .then((response) => response.json())
-          .then((result) => {
-            form.current.reset();
-            alert('Registration successful');
-          })
-          .catch((err) => alert('Registration failed'));
-      }
 
-  return (
-    <div>
-      <h2>Register</h2>
-      <label>Username:</label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <br />
-      <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <label>Role:</label>
-      <select value={role} onChange={(e) => setRole(e.target.value)}>
-        {roles.map((role) => (
-          <option key={role} value={role}>
-            {role}
-          </option>
-        ))}
-      </select>
-      <br />
-      <button onClick={handleRegister}>Register</button>
-    </div>
-  );
+        const fd = new FormData(form.current);
+        const fe = Object.fromEntries(fd.entries())
+
+        console.log('Frontend Data:', JSON.stringify(fe));
+
+        fetch('/api/db/register', {
+            method: 'POST',
+            body: JSON.stringify({ ...fe, role }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                form.current.reset();
+                setRole('MANAGER');
+                alert('Registration successful');
+            })
+            .catch((err) => alert('Registration failed',));
+    }
+
+    return (
+        <form id="form-register" onSubmit={onSubmitForm} ref={form} >
+            <h2>Register</h2>
+            <label>Username:</label>
+            <input type="text" name="username" required />
+            <br />
+            <label>Password:</label>
+            <input type="password" name="password" required />
+            <br />
+            <label>Role:</label>
+            <select value={onSubmitForm} onChange={(e) => setRole(e.target.value)}>
+                {roles.map((r) => (
+                    <option key={r} value={r} >
+                        {r}
+                    </option>
+                ))}
+            </select>
+            <br />
+            <button onClick={onSubmitForm}>Register</button>
+        </form>
+    );
 };
