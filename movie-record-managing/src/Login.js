20fdb80524a740853fb460c152c72f5efb9a8aa0
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-10-24 04:41:51 +0700

    Create Login.js

---
+++ movie-record-managing/src/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './style.css';
+
+export default function Login() {
+    const form = React.useRef();
+
+    const onSubmitForm = async (event) => {
+        event.preventDefault();
+
+        const fd = new FormData(form.current);
+        const fe = Object.fromEntries(fd.entries());
+
+        console.log('Frontend Data:', JSON.stringify(fe));
+
+        fetch('/api/db/login', {
+            method: 'POST',
+            body: JSON.stringify(fe),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                form.current.reset();
+                alert('Login successful');
+            })
+            .catch((err) => alert('Login failed',));
+    };
+
+    return (
+        <form id="form-login" onSubmit={onSubmitForm} ref={form}>
+            <h2>Login</h2>
+            <label>Username:</label>
+            <input type="text" name="username" required />
+            <br />
+            <label>Password:</label>
+            <input type="password" name="password" required />
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
