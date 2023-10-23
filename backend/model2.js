164f58d3a6fc45485597e8987d74d84479f780c4
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-10-24 04:41:50 +0700

    Update model2.js

--- backend/model2.js
+++ backend/model2.js
@@ -4,16 +4,22 @@ mongoose.connect('mongodb://0.0.0.0:27017/db1', {
     useNewUrlParser: true, useUnifiedTopology: true 
 });
 
-let userSchema = new mongoose.Schema({
-  username:String,
-  password:String,
-  role: {
-    type: String,
-    enum: ['MANAGER', 'TEAMLEADER', 'FLOORSTAFF'],
-    default: 'FLOORSTAFF',
-  },
-});
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
 
-let User = mongoose.model('User', userSchema);
+    },
+  });
+  
+  const User = mongoose.model('User', userSchema);
 
 module.exports = User;
